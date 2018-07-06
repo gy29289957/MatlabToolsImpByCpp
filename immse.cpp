dad322b0f12627855f647198ded945bc90ec490c
LZJ <40876286+gy29289957@users.noreply.github.com>
2018-07-06 11:47:57 +0800

    Create immse.cpp

---
+++ immse.cpp
@@ -0,0 +1,14 @@
+float immse(cv::Mat const &imgSrc1, cv::Mat const &imgSrc2)
+{
+	cv::Mat 		difference;
+	cv::Scalar 	s;
+
+	if(imgSrc1.size() != imgSrc2.size())
+		printf("calcMSE error: image size inconsistent\n");
+
+	cv::absdiff(imgSrc1, imgSrc2, difference);		//--- difference = |frame - background|
+	difference = difference.mul(difference);		//--- difference = difference^2
+	s = cv::sum(difference);						//--- sum
+
+	return (float)(s.val[0] / imgSrc1.total());
+}
